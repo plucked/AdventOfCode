3671c3f99d8d6c6b1f84be971c23bcd6e716dbfa
Rainer Bernhardt <rainer@klang-games.com>
2021-12-28 20:47:44 +0100

    2015-10 - Final

---
+++ AdventOfCode/Year2015/AoC2015Day10.cs
@@ -0,0 +1,46 @@
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2015;
+
+public class AoC2015Day10 {
+    private byte[] input;
+    private int iterations;
+
+    [GlobalSetup(Targets = new[] { nameof(Solution1) })]
+    public void BenchmarkSetup() {
+        Setup();
+    }
+
+    public void Setup(string? input = null, int iterations = 50) {
+        this.input = (input ?? "1321131112").Select(c => (byte)(c - '0')).ToArray();
+        this.iterations = iterations;
+    }
+
+    [Benchmark]
+    public long Solution1() {
+        var a = new List<byte>(input);
+        var b = new List<byte>();
+        for (int i = 0; i < iterations; i++) {
+            b.Clear();
+            byte last = a[0];
+            byte count = 1;
+            for (int j = 1; j < a.Count; j++) {
+                if (a[j] == last) {
+                    ++count;
+                    continue;
+                }
+
+                b.Add(count);
+                b.Add(last);
+                last = a[j];
+                count = 1;
+            }
+
+            b.Add(count);
+            b.Add(last);
+            (a, b) = (b, a);
+        }
+
+        return a.Count;
+    }
+}
---
+++ AdventOfCodeTests/Year2015/AoC2015Day10Tests.cs
@@ -0,0 +1,33 @@
+using System;
+using AdventOfCode.Year2015;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2015;
+
+[TestFixture]
+public class AoC2015Day10Tests {
+    [TestCase("1", 6)]
+    public void Solution1Example(string input, long expect) {
+        var instance = new AoC2015Day10();
+        instance.Setup(input, 5);
+        Assert.AreEqual(expect, instance.Solution1());
+    }
+
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2015Day10();
+        instance.Setup(null, 40);
+        var result = instance.Solution1();
+        Assert.AreEqual(492982, result);
+        Console.WriteLine($"Result: {result}");
+    }
+
+    [Test]
+    public void Solution2Test() {
+        var instance = new AoC2015Day10();
+        instance.Setup(null, 50);
+        var result = instance.Solution1();
+        Assert.AreEqual(6989950, result);
+        Console.WriteLine($"Result: {result}");
+    }
+}
