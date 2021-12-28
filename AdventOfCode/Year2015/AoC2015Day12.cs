46046cd027c45286f5855dae6966e9d063668e95
Rainer Bernhardt <rainer@klang-games.com>
2021-12-28 23:15:21 +0100

    2015-12 - Final

---
+++ AdventOfCode/Year2015/AoC2015Day12.cs
@@ -0,0 +1,126 @@
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2015;
+
+public class AoC2015Day12 {
+    private char[] input;
+
+    [GlobalSetup(Targets = new[] { nameof(Solution1), nameof(Solution2) })]
+    public void BenchmarkSetup() {
+        Setup();
+    }
+
+    public void Setup(string? line = null) {
+        input = (line ?? File.ReadAllText("Year2015/2015_12_input.txt")).ToCharArray();
+    }
+
+    [Benchmark]
+    public long Solution1() {
+        var result = 0L;
+        bool readingNumber = false;
+        long number = 0;
+        bool minusSign = false;
+        for (int i = 0; i < input.Length; i++) {
+            var c = input[i];
+            if (c >= '0' && c <= '9') {
+                if (readingNumber == false) {
+                    readingNumber = true;
+                    minusSign = input[i - 1] == '-';
+                    number = c - '0';
+                } else {
+                    number *= 10L;
+                    number += c - '0';
+                }
+            } else if (readingNumber) {
+                readingNumber = false;
+                if (minusSign) {
+                    number = -number;
+                }
+
+                result += number;
+                number = 0;
+            }
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public long Solution2() {
+        // we store the results per { } object so we can remove values when a "red" is detected
+        var objectResults = new List<long>();
+        objectResults.Add(0);
+
+        // remember which type the current object is
+        var objectStack = new Stack<char>();
+
+        bool readingNumber = false;
+        long number = 0;
+        bool minusSign = false;
+        int ignoreResultsIndex = -1;
+        for (int i = 0; i < input.Length; i++) {
+            var a = input[i];
+
+            // reading a number, early exit
+            if (a >= '0' && a <= '9') {
+                if (readingNumber == false) {
+                    readingNumber = true;
+                    minusSign = input[i - 1] == '-';
+                    number = a - '0';
+                } else {
+                    number *= 10L;
+                    number += a - '0';
+                }
+
+                continue;
+            }
+
+            // finish reading a number, add the result to the current object
+            if (readingNumber) {
+                readingNumber = false;
+                if (minusSign) {
+                    number = -number;
+                }
+
+                objectResults[^1] += number;
+                number = 0;
+            }
+
+            // check if there is a :"red" property coming
+            char? b = i + 1 < input.Length ? input[i + 1] : null;
+            char? c = i + 2 < input.Length ? input[i + 2] : null;
+            char? d = i + 3 < input.Length ? input[i + 3] : null;
+            char? e = i + 4 < input.Length ? input[i + 4] : null;
+            char? f = i + 5 < input.Length ? input[i + 5] : null;
+
+            if (ignoreResultsIndex == -1 && a == ':' && b == '"' && c == 'r' && d == 'e' && e == 'd' && f == '"' && objectStack.Peek() == '{') {
+                // remember the FIRST object depth saw this
+                ignoreResultsIndex = objectResults.Count - 1;
+            } else if (a == '[') {
+                // just to remember that we are in an array
+                objectStack.Push('[');
+            } else if (a == ']') {
+                objectStack.Pop();
+            } else if (a == '{') {
+                objectResults.Add(0);
+                objectStack.Push('{');
+            } else if (a == '}') {
+                objectStack.Pop();
+                // we don't add the result of an object to the parent object when it was marked as 
+                // ignored because we saw an "red" property value
+                if (ignoreResultsIndex != -1 && objectResults.Count - 1 > ignoreResultsIndex) {
+                } else if (ignoreResultsIndex != -1 && objectResults.Count - 1 == ignoreResultsIndex) {
+                    ignoreResultsIndex = -1;
+                } else if (ignoreResultsIndex == -1) {
+                    // add the value to the parent object results
+                    objectResults[^2] += objectResults[^1];
+                }
+
+                objectResults.RemoveAt(objectResults.Count - 1);
+            }
+        }
+
+        // the "root" object results are what we want to read 
+        return objectResults[0];
+    }
+}
---
+++ AdventOfCodeTests/Year2015/AoC2015Day12Tests.cs
@@ -0,0 +1,55 @@
+using System;
+using AdventOfCode.Year2015;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2015;
+
+[TestFixture]
+public class AoC2015Day12Tests {
+    [TestCase("[1,2,3]", 6)]
+    [TestCase("{\"a\":2,\"b\":4}", 6)]
+    [TestCase("[[[3]]]", 3)]
+    [TestCase("{\"a\":{\"b\":4},\"c\":-1}", 3)]
+    [TestCase("{\"a\":[-1,1]}", 0)]
+    [TestCase("[-1,{\"a\":1}]", 0)]
+    [TestCase("[]", 0)]
+    [TestCase("{}", 0)]
+    public void Solution1Example(string input, long expect) {
+        var instance = new AoC2015Day12();
+        instance.Setup(input);
+        Assert.AreEqual(expect, instance.Solution1());
+    }
+
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2015Day12();
+        instance.Setup();
+        var result = instance.Solution1();
+        Assert.AreEqual(191164, result);
+        Console.WriteLine($"Result: {result}");
+    }
+    
+    [TestCase("[1,2,3]", 6)]
+    [TestCase("[1,{\"c\":\"red\",\"b\":2},3]", 4)]
+    [TestCase("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", 0)]
+    [TestCase("[1,\"red\",5]", 6)]
+    [TestCase("{\"a\":1,b:[{\"c\":3,\"e\":\"red\"}]}", 1)]
+    [TestCase("[{\"a\":\"red\",\"b\":7},{c:8}]", 8)]
+    [TestCase("{\"a\":{\"b\":{\"c\":\"red\",\"d\":1},\"e\":2},\"f\":3}", 5)]
+    [TestCase("{\"a\":{\"b\":{\"d\":1},\"c\":\"red\",\"e\":2},\"f\":3}", 3)]
+    [TestCase("{\"a\":{\"b\":\"red\",\"c\":3},\"d\":\"red\",\"e\":3}", 0)]
+    public void Solution2Example(string input, long expect) {
+        var instance = new AoC2015Day12();
+        instance.Setup(input);
+        Assert.AreEqual(expect, instance.Solution2());
+    }
+
+    [Test]
+    public void Solution2Test() {
+        var instance = new AoC2015Day12();
+        instance.Setup();
+        var result = instance.Solution2();
+        Assert.AreEqual(87842, result);
+        Console.WriteLine($"Result: {result}");
+    }
+}
