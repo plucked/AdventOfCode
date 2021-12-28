8cc165c5a32419400da7264fae3c975887e75ac5
Rainer Bernhardt <rainer@klang-games.com>
2021-12-28 13:54:53 +0100

    2015-07 - Final

---
+++ AdventOfCode/Year2015/AoC2015Day07.cs
@@ -0,0 +1,148 @@
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2015;
+
+public class AoC2015Day07 {
+    private Instruction[] instructions;
+
+    struct Instruction {
+        public Op Operation;
+        public string? VariableA;
+        public uint? ValueA;
+        public string? VariableB;
+        public uint? ValueB;
+        public string Output;
+
+        public enum Op {
+            ASSIGN,
+            AND,
+            OR,
+            NOT,
+            LSHIFT,
+            RSHIFT
+        }
+    }
+
+    [GlobalSetup(Targets = new[] { nameof(Solution1), nameof(Solution2) })]
+    public void BenchmarkSetup() {
+        Setup();
+    }
+
+    public void Setup(string[]? customInput = null) {
+        var lines = customInput ?? File.ReadAllLines("Year2015/2015_07_input.txt");
+        instructions = lines.Select(
+                                    line => {
+                                        var split1 = line.Split(" -> ");
+                                        var instruction = new Instruction { Output = split1[1] };
+
+                                        if (Parse2Inputs(ref instruction, split1[0], "AND") == false &&
+                                            Parse2Inputs(ref instruction, split1[0], "OR") == false &&
+                                            Parse2Inputs(ref instruction, split1[0], "RSHIFT") == false &&
+                                            Parse2Inputs(ref instruction, split1[0], "LSHIFT") == false) {
+                                            if (line.Contains("NOT")) {
+                                                instruction.Operation = Instruction.Op.NOT;
+                                                var sub = split1[0].Substring("NOT ".Length);
+                                                if (uint.TryParse(sub, out var number)) {
+                                                    instruction.ValueA = number;
+                                                } else {
+                                                    instruction.VariableA = sub;
+                                                }
+                                            } else {
+                                                instruction.Operation = Instruction.Op.ASSIGN;
+                                                if (uint.TryParse(split1[0], out var number)) {
+                                                    instruction.ValueA = number;
+                                                } else {
+                                                    instruction.VariableA = split1[0];
+                                                }
+                                            }
+                                        }
+
+                                        return instruction;
+                                    })
+                            .ToArray();
+
+        bool Parse2Inputs(ref Instruction instruction, string line, string keyword) {
+            if (line.Contains(keyword)) {
+                instruction.Operation = Enum.Parse<Instruction.Op>(keyword);
+                var split2 = line.Split($" {keyword} ");
+                if (uint.TryParse(split2[0], out var number)) {
+                    instruction.ValueA = number;
+                } else {
+                    instruction.VariableA = split2[0];
+                }
+
+                if (uint.TryParse(split2[1], out number)) {
+                    instruction.ValueB = number;
+                } else {
+                    instruction.VariableB = split2[1];
+                }
+            } else {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    [Benchmark]
+    public Dictionary<string, uint> Solution1() {
+        var variables = new Dictionary<string, uint>();
+        var q = new Queue<Instruction>(instructions);
+
+        while (q.TryDequeue(out var instruction)) {
+            // don't run an instruction when the inputs are not calculated
+            if ((instruction.VariableA != null && variables.ContainsKey(instruction.VariableA) == false) ||
+                (instruction.VariableB != null && variables.ContainsKey(instruction.VariableB) == false)) {
+                q.Enqueue(instruction);
+                continue;
+            }
+
+            switch (instruction.Operation) {
+                case Instruction.Op.ASSIGN:
+                    variables[instruction.Output] = instruction.ValueA ?? variables[instruction.VariableA!];
+                    break;
+                case Instruction.Op.AND:
+                    variables[instruction.Output] = (instruction.ValueA ?? variables[instruction.VariableA!]) & (instruction.ValueB ?? variables[instruction.VariableB!]);
+                    break;
+                case Instruction.Op.OR:
+                    variables[instruction.Output] = (instruction.ValueA ?? variables[instruction.VariableA!]) | (instruction.ValueB ?? variables[instruction.VariableB!]);
+                    break;
+                case Instruction.Op.NOT:
+                    variables[instruction.Output] = ~(instruction.ValueA ?? variables[instruction.VariableA!]);
+                    variables[instruction.Output] &= 0b1111111111111111; // make sure we cap at 16bit
+                    break;
+                case Instruction.Op.LSHIFT:
+                    variables[instruction.Output] = (instruction.ValueA ?? variables[instruction.VariableA!]) << (int)(instruction.ValueB ?? variables[instruction.VariableB!]);
+                    variables[instruction.Output] &= 0b1111111111111111; // make sure we cap at 16bit
+                    break;
+                case Instruction.Op.RSHIFT:
+                    variables[instruction.Output] = (instruction.ValueA ?? variables[instruction.VariableA!]) >> (int)(instruction.ValueB ?? variables[instruction.VariableB!]);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        return variables;
+    }
+
+    [Benchmark]
+    public Dictionary<string, uint> Solution2() {
+        // run solution 1 to get a
+        var v = Solution1();
+        // replace the instruction which assigns "b" with the value from a
+        for (var i = 0; i < instructions.Length; i++) {
+            var instruction = instructions[i];
+            if (instruction.Operation != Instruction.Op.ASSIGN || instruction.Output != "b") {
+                continue;
+            }
+
+            instruction.ValueA = v["a"];
+            instructions[i] = instruction;
+            break;
+        }
+
+        // rerun it
+        return Solution1();
+    }
+}
---
+++ AdventOfCodeTests/Year2015/AoC2015Day07Tests.cs
@@ -0,0 +1,54 @@
+using System;
+using AdventOfCode.Year2015;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2015;
+
+[TestFixture]
+public class AoC2015Day07Tests {
+    [Test]
+    public void Solution1Example() {
+        var instance = new AoC2015Day07();
+        instance.Setup(@"123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i".Split(Environment.NewLine));
+        
+        var result = instance.Solution1();
+        foreach (var v in result) {
+            Console.WriteLine($"{v.Key}={v.Value}");
+        }
+
+        Assert.AreEqual(72, result["d"]);
+        Assert.AreEqual(507, result["e"]);
+        Assert.AreEqual(492, result["f"]);
+        Assert.AreEqual(114, result["g"]);
+        Assert.AreEqual(65412, result["h"]);
+        Assert.AreEqual(65079, result["i"]);
+        Assert.AreEqual(123, result["x"]);
+        Assert.AreEqual(456, result["y"]);
+    }
+    
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2015Day07();
+        instance.Setup();
+        var result = instance.Solution1()["a"];
+        Assert.AreEqual(46065, result);
+        Console.WriteLine($"Result: {result}");
+    }
+    
+
+    [Test]
+    public void Solution2Test() {
+        var instance = new AoC2015Day07();
+        instance.Setup();
+        var result = instance.Solution2()["a"];
+        Assert.AreEqual(14134, result);
+        Console.WriteLine($"Result: {result}");
+    }
+}
