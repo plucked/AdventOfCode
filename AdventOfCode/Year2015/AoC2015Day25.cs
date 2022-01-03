131d58d7d31cfa3cd20af05735538d4faff7f651
Rainer Bernhardt <rainer@klang-games.com>
2022-01-03 16:54:36 +0100

    Minor cleanup

--- AdventOfCode/Year2015/AoC2015Day25.cs
+++ AdventOfCode/Year2015/AoC2015Day25.cs
@@ -3,12 +3,6 @@ using BenchmarkDotNet.Attributes;
 namespace AdventOfCode.Year2015;
 
 public class AoC2015Day25 {
-    public ulong row = 3010;
-    public ulong column = 3019;
-    public ulong first = 20151125;
-    public ulong firstOp = 252533;
-    public ulong secondOp = 33554393;
-
     [GlobalSetup(Targets = new[] { nameof(Solution1) })]
     public void BenchmarkSetup() {
         Setup();
@@ -19,6 +13,12 @@ public class AoC2015Day25 {
 
     [Benchmark]
     public long Solution1() {
+        ulong row = 3010;
+        ulong column = 3019;
+        ulong first = 20151125;
+        ulong firstOp = 252533;
+        ulong secondOp = 33554393;
+
         var targetRow = row - 1 + column;
         // see https://en.wikipedia.org/wiki/Lazy_caterer%27s_sequence
         var end = targetRow * (targetRow - 1) / 2ul + 1ul;
