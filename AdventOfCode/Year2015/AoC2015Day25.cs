f18528b4d91d548c50243258c0092e234b0554da
Rainer Bernhardt <rainer@klang-games.com>
2022-01-03 16:50:10 +0100

    2015-25 - Final

---
+++ AdventOfCode/Year2015/AoC2015Day25.cs
@@ -0,0 +1,33 @@
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2015;
+
+public class AoC2015Day25 {
+    public ulong row = 3010;
+    public ulong column = 3019;
+    public ulong first = 20151125;
+    public ulong firstOp = 252533;
+    public ulong secondOp = 33554393;
+
+    [GlobalSetup(Targets = new[] { nameof(Solution1) })]
+    public void BenchmarkSetup() {
+        Setup();
+    }
+
+    public void Setup() {
+    }
+
+    [Benchmark]
+    public long Solution1() {
+        var targetRow = row - 1 + column;
+        // see https://en.wikipedia.org/wiki/Lazy_caterer%27s_sequence
+        var end = targetRow * (targetRow - 1) / 2ul + 1ul;
+        end += column - 2;
+        ulong prevRow = first;
+        for (ulong i = 1; i <= end; i++) {
+            prevRow = (prevRow * firstOp) % secondOp;
+        }
+
+        return (long)prevRow;
+    }
+}
---
+++ AdventOfCodeTests/Year2015/AoC2015Day25Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using AdventOfCode.Year2015;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2015;
+
+[TestFixture]
+public class AoC2015Day25Tests {
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2015Day25();
+        instance.Setup();
+        var result = instance.Solution1();
+        Assert.AreEqual(8997277, result);
+        Console.WriteLine($"Result: {result}");
+    }
+}
