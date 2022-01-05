dd77a8e1bcd5745cda805b8734f6ed07b9531499
Rainer Bernhardt <rainer@klang-games.com>
2022-01-05 11:18:46 +0100

    Some benchmark config

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,13 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
 
 public static class Program {
     private static void Main(string[] args) {
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        var config = ManualConfig.CreateMinimumViable().AddJob(Job.Default).AddExporter(MarkdownExporter.Default).AddDiagnoser(MemoryDiagnoser.Default);
+
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
     }
 }
