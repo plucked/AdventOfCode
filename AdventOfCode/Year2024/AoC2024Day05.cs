b93c4ee80ecc8c97593d9711dc80aa5b72a0f40a
Rainer Bernhardt <rainer.bernhardt@plucked.de>
2024-12-15 15:21:28 +0100

    day 5 2024

---
+++ AdventOfCode/Year2024/AoC2024Day05.cs
@@ -0,0 +1,105 @@
+﻿using AdventOfCode.Utilities;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2024;
+
+public class AoC2024Day05
+{
+    // input is negative when line is empty
+    private readonly (int, int)[] orderRules;
+    private readonly int[][] orders;
+    
+    public AoC2024Day05(string? customInput = null)
+    {
+        var content = (customInput ?? EmbeddedInput.ReadAllText("Year2024/2024_05_input.txt"));
+        var lines = content.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        orderRules = new (int, int)[lines.Count(l => l.Contains('|'))];
+        orders = new int[lines.Length - orderRules.Length][];
+        for (int i = 0; i < orderRules.Length; i++)
+        {
+            var separatorIndex = lines[i].IndexOf('|');
+            orderRules[i] = (int.Parse(lines[i][..separatorIndex]), int.Parse(lines[i][(separatorIndex + 1)..]));
+        }
+
+        for (int i = 0; i < orders.Length; i++)
+        {
+            var line = lines[i + orderRules.Length];
+            var parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            orders[i] = parts.Select(int.Parse).ToArray();
+        }
+    }
+
+    [Benchmark]
+    public long Solution1()
+    {
+        int sum = 0;
+        foreach (var order in orders)
+        {
+            var orderSortedCorrectly = new int[order.Length];
+            Array.Copy(order, orderSortedCorrectly, order.Length);
+            Array.Sort(orderSortedCorrectly, (a, b) =>
+            {
+                foreach (var rule in orderRules)
+                {
+                    if (rule.Item1 == a && rule.Item2 == b)
+                    {
+                        return -1;
+                    }
+                    else if (rule.Item1 == b && rule.Item2 == a)
+                    {
+                        return 1;
+                    }
+                }
+
+                return 0;
+            });
+            
+            if (order.SequenceEqual(orderSortedCorrectly))
+            {
+                var middle = order.Length / 2;
+                var mid = order[middle];
+                sum += mid;
+            }
+        }
+
+        return sum;
+    }
+
+    
+
+    [Benchmark]
+    public long Solution2()
+    {
+        int sum = 0;
+        foreach (var order in orders)
+        {
+            var orderSortedCorrectly = new int[order.Length];
+            Array.Copy(order, orderSortedCorrectly, order.Length);
+            Array.Sort(orderSortedCorrectly, (a, b) =>
+            {
+                foreach (var rule in orderRules)
+                {
+                    if (rule.Item1 == a && rule.Item2 == b)
+                    {
+                        return -1;
+                    }
+                    else if (rule.Item1 == b && rule.Item2 == a)
+                    {
+                        return 1;
+                    }
+                }
+
+                return 0;
+            });
+            
+            if (!order.SequenceEqual(orderSortedCorrectly))
+            {
+                var middle = orderSortedCorrectly.Length / 2;
+                var mid = orderSortedCorrectly[middle];
+                sum += mid;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ AdventOfCodeTests/Year2024/AoC2024Day05Tests.cs
@@ -0,0 +1,103 @@
+﻿using AdventOfCode.Year2024;
+using System;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2024;
+
+[TestFixture]
+public class AoC2024Day05Tests {
+    [Test]
+    public void Solution1Examples()
+    {
+        string input = """
+                       47|53
+                       97|13
+                       97|61
+                       97|47
+                       75|29
+                       61|13
+                       75|53
+                       29|13
+                       97|29
+                       53|29
+                       61|53
+                       97|53
+                       61|29
+                       47|13
+                       75|47
+                       97|75
+                       47|61
+                       75|61
+                       47|29
+                       75|13
+                       53|13
+                       
+                       75,47,61,53,29
+                       97,61,53,29,13
+                       75,29,13
+                       75,97,47,61,53
+                       61,13,29
+                       97,13,75,29,47
+                       """;
+        
+        long expect = 143;
+        
+        var instance = new AoC2024Day05(input);
+        Assert.That(instance.Solution1(), Is.EqualTo(expect));
+    }
+
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2024Day05();
+        var result = instance.Solution1();
+        Assert.That(result, Is.EqualTo(5639));
+        Console.WriteLine($"Result: {result}");
+    }
+
+    [Test]
+    public void Solution2Examples() {
+        string input = """
+                       47|53
+                       97|13
+                       97|61
+                       97|47
+                       75|29
+                       61|13
+                       75|53
+                       29|13
+                       97|29
+                       53|29
+                       61|53
+                       97|53
+                       61|29
+                       47|13
+                       75|47
+                       97|75
+                       47|61
+                       75|61
+                       47|29
+                       75|13
+                       53|13
+                       
+                       75,47,61,53,29
+                       97,61,53,29,13
+                       75,29,13
+                       75,97,47,61,53
+                       61,13,29
+                       97,13,75,29,47
+                       """;
+        
+        long expect = 123;
+        
+        var instance = new AoC2024Day05(input);
+        Assert.That(instance.Solution2(), Is.EqualTo(expect));
+    }
+
+    [Test]
+    public void Solution2Test() {
+        var instance = new AoC2024Day05();
+        var result = instance.Solution2();
+        Assert.That(result, Is.EqualTo(5273));
+        Console.WriteLine($"Result: {result}");
+    }
+}
