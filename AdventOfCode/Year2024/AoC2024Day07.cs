b7ad539e9613b546a87b27bc99eef90c3cab2c30
Rainer Bernhardt <rainer.bernhardt@plucked.de>
2024-12-15 16:53:46 +0100

    day 7 2024

---
+++ AdventOfCode/Year2024/AoC2024Day07.cs
@@ -0,0 +1,116 @@
+﻿using System.Numerics;
+using AdventOfCode.Utilities;
+using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode.Year2024;
+
+public class AoC2024Day07
+{
+    // input is negative when line is empty
+    private readonly (long result, long[] values)[] tests;
+    
+    public AoC2024Day07(string? customInput = null)
+    {
+        var content = (customInput ?? EmbeddedInput.ReadAllText("Year2024/2024_07_input.txt"));
+        var lines = content.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        tests = new (long, long[])[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var colonIndex = lines[i].IndexOf(':');
+            var testResult = long.Parse(lines[i][..colonIndex]);
+            var test = lines[i][(colonIndex + 2)..].Split(' ').Select(long.Parse).ToArray();
+            tests[i] = (testResult, test);
+        }
+    }
+
+    [Benchmark]
+    public long Solution1()
+    {
+        long correctSum = 0;
+
+        foreach (var test in tests)
+        {
+            var testResult = test.result;
+            var testValues = test.values;
+            
+            uint operatorFlags = 0; // bit field 0 = +, 1 = *
+            while (BitOperations.PopCount(operatorFlags) < testValues.Length)
+            {
+                var result = testValues[0];
+                for (int i = 1; i < testValues.Length; i++)
+                {
+                    if ((operatorFlags & (1u << i)) == 0)
+                    {
+                        result += testValues[i];
+                    }
+                    else
+                    {
+                        result *= testValues[i];
+                    }
+                }
+
+                if (result == testResult)
+                {
+                    correctSum += testResult;
+                    break;
+                }
+                
+                operatorFlags++;
+            }
+        }
+        
+        return correctSum;
+    }
+
+    [Benchmark]
+    public long Solution2()
+    {
+        long correctSum = 0;
+
+        foreach (var test in tests)
+        {
+            var testResult = test.result;
+            var testValues = test.values;
+            
+            uint operatorFlags = 0; // bit field 00 = +, 01 = *, 10 = concat, 11 = skip
+            while (BitOperations.PopCount(operatorFlags) < testValues.Length * 2)
+            {
+                var result = testValues[0];
+                bool skip = false;
+                for (int i = 1; i < testValues.Length; i++)
+                {
+                    var op = (operatorFlags >> (i * 2)) & 3u;
+                    if (op == 0)
+                    {
+                        result += testValues[i];
+                    }
+                    else if (op == 1)
+                    {
+                        result *= testValues[i];
+                    }
+                    else if (op == 2)
+                    {
+                        result = long.Parse(result.ToString() + testValues[i]);
+                    }
+                    else
+                    {
+                        skip = true;
+                        break;
+                    }
+                }
+
+                if (skip == false && result == testResult)
+                {
+                    correctSum += testResult;
+                    break;
+                }
+                
+                operatorFlags++;
+            }
+        }
+        
+        return correctSum;
+    }
+    
+    
+}
---
+++ AdventOfCodeTests/Year2024/AoC2024Day07Tests.cs
@@ -0,0 +1,65 @@
+﻿using AdventOfCode.Year2024;
+using System;
+using NUnit.Framework;
+
+namespace AdventOfCodeTests.Year2024;
+
+[TestFixture]
+public class AoC2024Day07Tests {
+    [Test]
+    public void Solution1Examples()
+    {
+        string input = """
+                       190: 10 19
+                       3267: 81 40 27
+                       83: 17 5
+                       156: 15 6
+                       7290: 6 8 6 15
+                       161011: 16 10 13
+                       192: 17 8 14
+                       21037: 9 7 18 13
+                       292: 11 6 16 20
+                       """;
+        
+        long expect = 3749;
+        
+        var instance = new AoC2024Day07(input);
+        Assert.That(instance.Solution1(), Is.EqualTo(expect));
+    }
+
+    [Test]
+    public void Solution1Test() {
+        var instance = new AoC2024Day07();
+        var result = instance.Solution1();
+        Assert.That(result, Is.EqualTo(7710205485870));
+        Console.WriteLine($"Result: {result}");
+    }
+
+    [Test]
+    public void Solution2Examples() {
+        string input = """
+                       190: 10 19
+                       3267: 81 40 27
+                       83: 17 5
+                       156: 15 6
+                       7290: 6 8 6 15
+                       161011: 16 10 13
+                       192: 17 8 14
+                       21037: 9 7 18 13
+                       292: 11 6 16 20
+                       """;
+        
+        long expect = 11387;
+        
+        var instance = new AoC2024Day07(input);
+        Assert.That(instance.Solution2(), Is.EqualTo(expect));
+    }
+
+    [Test]
+    public void Solution2Test() {
+        var instance = new AoC2024Day07();
+        var result = instance.Solution2();
+        Assert.That(result, Is.EqualTo(20928985450275));
+        Console.WriteLine($"Result: {result}");
+    }
+}
